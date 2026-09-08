7dcb747884d50bacdb9f9aa16540fa24acd2dda6
Hernan <62639053+Hernan-F22@users.noreply.github.com>
2026-09-08 12:35:50 +0700

    Add files via upload

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -18,6 +18,13 @@ $pageTitle = 'Admin Dashboard - ' . APP_NAME;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($pageTitle) ?></title>
 
+    <!-- Favicon & App Icons -->
+    <link rel="icon" type="image/svg+xml" href="../favicon.svg">
+    <link rel="icon" type="image/png" sizes="32x32" href="../favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="../apple-touch-icon.png">
+    <link rel="shortcut icon" href="../favicon.ico">
+
     <!-- Google Fonts & FontAwesome 6 -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
